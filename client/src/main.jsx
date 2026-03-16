c48bd195d576011ef723aca114afead690685153
emeralddd <tunglambinbin@gmail.com>
2026-03-16 15:13:47 +1100

    using react-query to handle api call for frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,14 @@ import './css/addition.css';
 import './css/animation.css';
 import './css/prism-vsc-dark-plus.css';
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </StrictMode>,
+);
