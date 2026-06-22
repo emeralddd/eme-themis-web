b201294aee72a20c956fda3112fa6aed32fcb234
emeralddd <tunglambinbin@gmail.com>
2026-06-22 21:42:43 +1000

    fix: using correct url of api

--- client/src/utils/VariableName.jsx
+++ client/src/utils/VariableName.jsx
@@ -1,6 +1,6 @@
-export const apiURL = import.meta.env.VITE_BACKEND_URL + '/api'
+export const hostURL = import.meta.env.DEV ? '' : import.meta.env.VITE_BACKEND_URL
 
-export const hostURL = import.meta.env.VITE_BACKEND_URL
+export const apiURL = hostURL + '/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'themis'
 
